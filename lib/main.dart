543296b3c8162c61ec4ceda33db342bd46e85506
Sanpetch Boriboon <sunpetch1997@gmail.com>
2026-02-06 11:21:25 +0700

    Remove flutter_dotenv dependency and simplify main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'font_styles.dart';
 import 'gallery.dart';
@@ -13,8 +12,7 @@ import 'wishes.dart';
 // Global color constant
 const Color kPrimaryColor = Color(0xFF7E8B78);
 
-Future<void> main() async {
-  await dotenv.load(fileName: ".env");
+void main() {
   runApp(const MyApp());
 }
 
